37790f6e2d73dddf2c7e970d112fa263db4e0c99
Undec1ded <savyuk.v.o@gmail.com>
2024-04-11 15:32:50 +0300

    added simulation module SPI_test.sv for SPI.sv and fixed module SPI.sv

--- Bachelors_thesis.srcs/sources_1/new/SPI.sv
+++ Bachelors_thesis.srcs/sources_1/new/SPI.sv
@@ -2,14 +2,14 @@
 module SPI#(
     parameter data_hier_bit = 16
 )(
-    input SCLK, [data_hier_bit - 1 : 0] DATA, SDI_flag,
+    input SCLK, SDI_flag, [data_hier_bit - 1 : 0] DATA, 
 
     output SDI, reg END_spi
 );
 
 reg SDI_transfer = 0;
 reg [4 : 0] bits_counter_16 = 0;
-reg [data_hier_bit - 1 : 0] data_amplitude;
+reg [data_hier_bit - 1 : 0] data_amplitude = 0;
 reg counter_flag = 0;
 
 assign SDI = SDI_transfer;
@@ -17,20 +17,21 @@ assign END_spi = counter_flag;
 
 always @(SCLK) begin
     if (SDI_flag == 0) begin
-        data_amplitude = DATA;
-        bits_counter_16 = 0;
+        data_amplitude <= DATA;
+        bits_counter_16 <= 0;
+        counter_flag <= 0;
     end
     else begin
         if (bits_counter_16 != 16) begin
-            bits_counter_16 = bits_counter_16 + 1;
-            counter_flag = 0;
+            bits_counter_16 <= bits_counter_16 + 1;
+            counter_flag <= 0;
+            SDI_transfer <= data_amplitude[data_hier_bit - 1];
+            data_amplitude = data_amplitude << 1;
         end
         else begin
-            bits_counter_16 = 0;
-            counter_flag = 1;
+            bits_counter_16 <= 0;
+            counter_flag <= 1;
         end
-        SDI_transfer = data_amplitude[data_hier_bit - 1];
-        data_amplitude = data_amplitude << 1;
     end
 end
 
